3e7fa2a0ea8d29f34e4c5598f53b16bba8b28dd0
geonuklee <kunwooklee27@gmail.com>
2021-12-02 15:56:09 +0900

    Ready to generate label.

--- scripts/unet/generate_dataset.py
+++ scripts/unet/generate_dataset.py
@@ -15,19 +15,34 @@ import numpy as np
 
 #import ros_numpy # ros_numpy doesn't support numpify for compressed rosbag topic
 from cv_bridge import CvBridge
+from os import makedirs
 
 
 '''
 TODO
 0) From ground truth list
 0) Ask user to overwrite exist frame, if it is already exist frame.
-1) Collect all /cam*/k4a/depth/image_raw, and rgb or rgb_to_depth for it.
-2) Thresholding laplacian of depth
+Done) Collect all /cam*/k4a/depth/image_raw, and rgb or rgb_to_depth for it.
+Done) Thresholding laplacian of depth
 3) Save edge - rgb. (rgb for reference)
 4) Call ' kolourpaint ground_truth.png'
 5) Convert above to edge.npy
 6) Add 'filename' to filename list.
 
+    script_fn = osp.abspath(__file__)
+    pkg_dir = str('/').join(script_fn.split('/')[:-2])
+    dataset_path = osp.join(pkg_dir, 'edge_dataset')
+    usages = ['train', 'valid', 'test']
+    usage_nframes = {'train':0.8*len(dataset),
+        'valid':0.1*len(dataset),
+        'test':0.1*len(dataset) }
+
+    if not osp.exists(dataset_path):
+        makedirs(dataset_path)
+        for usage in usages:
+            usagepath = osp.join(dataset_path,usage)
+            if not osp.exists(usagepath):
+                makedirs(usagepath)
 
 
 
@@ -43,14 +58,47 @@ def get_topic(filename, topic):
 
 if __name__ == '__main__':
     # ref) https://www.notion.so/c-Python-Rosbag-API-88b49f2c413f40e1b3e8d2f09894c6bf
-    # TODO hardcoding
-    rosbag_path = '/home/geo/dataset/unloading/**/*.bag'
-    files = glob2.glob(rosbag_path,recursive=True)
+    rosbag_path = '/home/geo/dataset/unloading/**/*.bag' # remove hardcoding .. 
+    rosbagfiles = glob2.glob(rosbag_path,recursive=True)
+
+    script_fn = osp.abspath(__file__)
+    pkg_dir = str('/').join(script_fn.split('/')[:-3])
+    dataset_path = osp.join(pkg_dir, 'segment_dataset')
+    print(dataset_path) 
+
+    usages = {'train':0.9, 'valid':0.1}
+    n_frames = {}
+    if not osp.exists(dataset_path):
+        makedirs(dataset_path)
+        for usage in usages.keys():
+            usagepath = osp.join(dataset_path,usage)
+            makedirs(usagepath)
+            n_frames[usage] = 0
+    else:
+        # Count file list
+        for usage in usages.keys():
+            depth_files = osp.join(dataset_path,usage,'*_depth.npy')
+            n_frame = len(glob2.glob(depth_files))
+            print(n_frame)
+            n_frames[usage] = n_frame
+            import pdb; pdb.set_trace()
 
     bridge = CvBridge()
-    closed_set = set()
-    for fullfn in files:
-        fn = osp.basename(fullfn,)
+    fn_rosbag_list = osp.join(dataset_path,'rosbag_list.txt')
+    if osp.exists(fn_rosbag_list):
+        closed_set_fp = open(fn_rosbag_list,'r')
+        closed_set = closed_set_fp.read().split("\n")
+        closed_set_fp.close()
+        closed_set = filter(lambda fn: fn != '', closed_set)
+        closed_set = set(closed_set)
+    else:
+        closed_set = set()
+
+    fp_closed_set = open(fn_rosbag_list,'a')
+
+    for fullfn in rosbagfiles:
+        fn = osp.basename(fullfn)
+        #print(fn.split(".")[0])
         if fn in closed_set:
             continue
         closed_set.add(fn)
@@ -81,12 +129,38 @@ if __name__ == '__main__':
             lap5 = cv2.Laplacian(depth, cv2.CV_32FC1, ksize=5)
             lap5 = cv2.resize(lap5,(1280,960))
 
-            print(topic, cam_id, camera_type, depth_type)
+            #print(topic, cam_id, camera_type, depth_type)
+            if n_frames['train'] < usages['train']*len(closed_set) :
+                usage = 'train'
+            else:
+                usage = 'valid'
+
+            usagepath = osp.join(dataset_path,usage)
+            fn_depth = osp.join(usagepath,'%d_depth.npy'%n_frames[usage])
+            fn_info  = osp.join(usagepath,'%d_info.txt'%n_frames[usage])
+            fp_info = open(fn_info, 'w')
+            line = '\t'.join([fn, topic, cam_id, camera_type, depth_type])
+
+            fp_info.write(line+"\n")
+            fp_info.flush()
+
+            fp_closed_set.write(fn+"\n")
+            fp_closed_set.flush()
+
+            n_frames[usage] += 1
+
             cv2.imshow("lap3", (lap3<-0.01).astype(np.uint8)*255)
             cv2.imshow("lap5", (lap5<-0.1 ).astype(np.uint8)*255)
             cv2.imshow("rgb", rgb)
             c = cv2.waitKey()
             if c == ord('q'):
                 exit(1)
+
+
+            # TODO write image
+            # TODO kolourpaint iamge
+            # TODO update rosbaglist.txt
+            # TODO show instance map with component 
+
 
 
