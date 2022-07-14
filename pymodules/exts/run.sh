4e7adf41e0ce773c2a17c141acde65e3116849df
geonuklee <kunwooklee27@gmail.com>
2022-07-14 23:31:37 +0900

    Resolve dependency collision of pcl::segmentation and libpython3

--- pymodules/exts/run.sh
+++ pymodules/exts/run.sh
@@ -1,5 +1,5 @@
-rm ~/.local/lib/python2.7/site-packages/unet*
-rm ~/.local/lib/python3.6/site-packages/unet*
+rm -rf ~/.local/lib/python2.7/site-packages/unet*
+rm -rf ~/.local/lib/python3.6/site-packages/unet*
 
 CWD=$PWD
 rm -rf build2; mkdir build2 && cd build2
@@ -10,6 +10,6 @@ rm -rf build3; mkdir build3 && cd build3
 cmake ../py3ext -DCMAKE_BUILD_TYPE=Release && make -j8
 cd $CWD
 
-echo "Check installation of unet"
+#echo "Check installation of unet"
 python2 -c "import unet_ext; print(unet_ext.__file__)"
 python3 -c "import unet_ext; print(unet_ext.__file__)"
